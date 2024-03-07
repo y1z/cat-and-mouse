dd3e36835f9be0abc628ccfff222a6b68cb8ad53
y1z <whyis2cool@gmail.com>
2024-03-06 19:31:05 -0600

    Add "SmoothCameraFollow " script

---
+++ Assets/Scripts/Camera/SmoothCameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FishNet.Object;
+using UnityEngine;
+
+namespace Camera
+{
+    public class SmoothCameraFollow : NetworkBehaviour
+    {
+
+        private Vector3 _offset;
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _smoothTime;
+        private Vector3 _currentVelocity = Vector3.zero;
+
+        private void Awake()
+        {
+            _offset = transform.position - _target.position;
+        }
+
+
+        private void LateUpdate()
+        {
+            Vector3 targetPosition = _target.position + _offset;
+            transform.position =
+                Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, _smoothTime);
+        }
+    }
+}
