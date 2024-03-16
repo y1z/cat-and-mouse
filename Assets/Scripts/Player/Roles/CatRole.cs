4e0a94e9fb6d03079d05cbfe5c009dc630974627
y1z <whyis2cool@gmail.com>
2024-03-15 20:05:28 -0600

    Add bare bones CatRole for debugging

---
+++ Assets/Scripts/Player/Roles/CatRole.cs
@@ -0,0 +1,25 @@
+﻿    public class CatRole : IRole
+    {
+        public bool OnInit(GeneralPlayerCharacteristics player)
+        {
+           player.ChangeColor(Globals.DEFAULT_CAT_COLOR);
+           return true;
+        }
+        
+
+        public bool OnUpdate(GeneralPlayerCharacteristics player)
+        {
+
+
+            return true;
+        }
+
+        public void DoRoleSpecialAction(GeneralPlayerCharacteristics player)
+        {
+        }
+
+        public bool OnEnd(GeneralPlayerCharacteristics player)
+        {
+            return true;
+        }
+    }
