af143315a028fbd4e9150beb22fcf5eb0b5faff2
y1z <whyis2cool@gmail.com>
2024-04-01 19:50:15 -0600

    Add utility file

---
+++ Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Color = UnityEngine.Color;
+
+namespace Utility
+{
+    public static class Utility
+    {
+
+        public static string addSizeTagToString(string input, int size)
+        {
+            string str_size = size.ToString();
+
+            return $"<size={str_size}> {input} </size>";
+        }
+
+        public static string addColorToString(string input, Color color)
+        {
+            string color_str = ColorUtility.ToHtmlStringRGBA(color);
+
+            return $"<color=#{color_str}> {input} </color>";
+        }
+        
+    }
+}
