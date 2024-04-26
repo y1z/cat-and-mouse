93f3406f00f2a77af8ca78539d740190be764a35
y1z <whyis2cool@gmail.com>
2024-04-25 22:10:08 -0600

    Add scripts for the playflow service

---
+++ Assets/Scripts/ScriptsForTheClass/PlayFlow/PartidasManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PartidasManager : MonoBehaviour
+{
+    [SerializeField] private PlayflowClientRequest _playflowClientRequest;
+
+    [SerializeField] private Transform servidoresContent;
+    [SerializeField] private Transform servidoreUiPrefab;
+
+    public void CrearPartida()
+    {
+
+        _playflowClientRequest.StartServer("us-west");
+    }
+
+
+    public void RefrescarListaPartidas()
+    {
+        _playflowClientRequest.GetServers( OnListaDeSererFetch );
+    }
+
+    void OnListaDeSererFetch(PlayflowClientRequest.ServerList serverList)
+    {
+
+        for (int i = servidoresContent.childCount - 1; i >= 0; --i)
+        {
+            Destroy(servidoresContent.GetChild(i).gameObject);
+        }
+        
+
+        foreach (var server in serverList.servers)
+        {
+            var serverUiGo = Instantiate(servidoreUiPrefab);
+        }
+        
+    }
+    
+}
